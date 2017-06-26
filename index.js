8d7c44bf79214132ac36744f1087850a785177b4
Chanon Onman <chanon@taskworld.com>
2017-06-26 16:31:03 +0700

    handle logout response

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Boom = require('boom')
+const Joi = require('joi')
 const server = require('./server')()
 
 // we ned to implement our own login callback to get more data
@@ -15,58 +16,110 @@ server.route({
   }
 })
 
+const _preprocessSaml = (req, reply) => {
+  const plugin = req.server.plugins['hapi-saml-sso'];
+  const saml = plugin && plugin['saml-instance'];
+  if (saml) {
+    const SAMLRequest = req.payload.SAMLRequest
+    const SAMLResponse = req.payload.SAMLResponse
+    if (SAMLRequest) {
+      // TODO Not supported
+      return reply(Boom.notAcceptable('SAMLRequest not supported'));
+    }
+    if (SAMLResponse) {
+      const xmldom = require('xmldom')
+      var xml = new Buffer(SAMLResponse, 'base64').toString('utf8');
+      var doc = new xmldom.DOMParser().parseFromString(xml);
+      console.log('------------ RAW (Base64) -----------')
+      console.log(SAMLResponse)
+      console.log('---------- User data (xml) ----------')
+      console.log(xml)
+      console.log('-------------------------------------')
+      saml.validatePostResponse(req.payload, (err, profile) => {
+        if (err !== null) {
+          console.log(err)
+          return reply(Boom.unauthorized());
+        }
+        return reply(profile);
+      });
+    }
+    else {
+      return reply(Boom.notAcceptable('Invalid SAML format'));
+    }
+  }
+  else {
+    return reply(Boom.badImplementation('SAML instance not exist'));
+  }
+}
+
+server.route({
+  method: 'GET',
+  path: '/_saml-logout',
+  config: {
+    auth: false,
+    validate: {
+      options: { stripUnknown: true },
+      query: {
+        nameIdFormat: Joi.string().required(),
+        nameId: Joi.string().required()
+      }
+    },
+  },
+  handler: function (request, reply) {
+    const plugin = request.server.plugins['hapi-saml-sso'];
+    const saml = plugin && plugin['saml-instance'];
+    if (saml) {
+      const options = {
+        user: {
+          nameIDFormat: request.query.nameIdFormat,
+          nameID: request.query.nameId
+        }
+      }
+      saml.getLogoutUrl(options, (err, logoutUrl) => {
+        if (err) {
+          return reply(Boom.wrap(err, 500))
+        }
+        reply.redirect(logoutUrl)
+      });
+    }
+    else {
+      reply(Boom.badImplementation('SAML instance not exist'))
+    }
+  }
+})
+
+server.route({
+  method: 'POST',
+  path: '/_saml-logout',
+  handler: (request, reply) => {
+    console.log('POST /_saml-logout')
+    return reply({})
+  }
+})
+
 server.route({
   method: 'POST',
   path: '/saml-callback',
   config: {
     auth: false,
-    pre: [
-      {
-        method: (req, reply) => {
-          const plugin = req.server.plugins['hapi-saml-sso'];
-          const saml = plugin && plugin['saml-instance'];
-          if (saml) {
-            const { SAMLRequest, SAMLResponse } = req.payload;
-            if (SAMLRequest) {
-              // TODO Not supported
-              return reply(Boom.notAcceptable('SAMLRequest not supported'));
-            }
-            if (SAMLResponse) {
-              const xmldom = require('xmldom')
-              var xml = new Buffer(SAMLResponse, 'base64').toString('utf8');
-              var doc = new xmldom.DOMParser().parseFromString(xml);
-              console.log('------------ RAW (Base64) -----------')
-              console.log(SAMLResponse)
-              console.log('---------- User data (xml) ----------')
-              console.log(xml)
-              console.log('-------------------------------------')
-              saml.validatePostResponse(req.payload, (err, profile) => {
-                if (err !== null) {
-                  return reply(Boom.unauthorized());
-                }
-                return reply(profile);
-              });
-            }
-            else {
-              return reply(Boom.notAcceptable('Invalid SAML format'));
-            }
-          }
-          else {
-            return reply(Boom.badImplementation('SAML instance not exist'));
-          }
-        },
-        assign: 'smalUser'
-      },
-    ],
+    pre: [{ method: _preprocessSaml, assign: 'samlUser' }],
   },
   handler: function (request, reply) {
-    const { smalUser } = request.pre
-    console.log('smalUser', request.pre.smalUser)
-    return reply().redirect(`taskworld://saml-callback?email=${smalUser.nameID}`)
+    const { samlUser } = request.pre
+    console.log('samlUser', samlUser) //, { getAssertionXml: samlUser.getAssertionXml() })
+    return reply({
+      samlUser,
+      loginUrl: 'https://adfs.taskworld.com/adfs/ls/idpinitiatedsignon.aspx',
+      logout1: 'https://adfs-test.taskworld.com:8000/adfs/ls/?wa=wsignout1.0',
+      logout2: 'https://adfs.taskworld.com/adfs/ls/?wa=wsignout1.0',
+      logout3: `https://adfs-test.taskworld.com:8000/_saml-logout?nameId=${samlUser.Email}&nameIdFormat=urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress`,
+      logout4: `https://adfs-test.taskworld.com:8000/saml/logout?nameId=${samlUser.Email}&nameIdFormat=urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress`
+    })
+    // return reply().redirect(`taskworld://saml-callback?email=${samlUser.nameID}`)
     // reply(`<script>${setSAMLLoggedIn}${refreshPage}</script>`)
     // return reply().redirect('https://cordova.taskworld.com/saml-callback')
     // reply({
-    //   smalUser,
+    //   samlUser,
     //   loginLink: 'https://dev.taskworld.com:8000/saml/login',
     //   logoutLink: 'https://taskworld-com-dev.onelogin.com/logout'
     // })
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const Hapi = require('hapi')
 const Fs = require('fs')
 
 // const SAML_DEFAULT_IDP = 'https://taskworld-com-dev.onelogin.com/trust/saml2/http-post/sso/670648'
-const SAML_DEFAULT_IDP = 'https://taskworld-com-dev.onelogin.com/trust/saml2/http-post/sso/671606'
+// const SAML_DEFAULT_IDP = 'https://taskworld-com-dev.onelogin.com/trust/saml2/http-post/sso/671606'
+const SAML_DEFAULT_IDP = 'https://adfs.taskworld.com/adfs/ls/'
 const ONELOGIN_CERT = '/Users/chanon/tw-secrets/onelogin.pem'
 const DECRYPT_KEY = './secret/idp-private-key.pem'
 const HOST = 'dev.taskworld.com'
@@ -16,11 +17,14 @@ function getServer () {
       register: require('hapi-saml-sso'),
       options: {
         callbackUrl: `https://${HOST}:${PORT}/saml-callback`,
-        // callbackUrl: 'https://edf5489a.ngrok.io/saml-callback',
+        // callbackUrl: 'https://a2adb161.ngrok.io/saml-callback',
         entryPoint: SAML_DEFAULT_IDP,
-        issuer: 'passport-saml',
-        cert: Fs.readFileSync(ONELOGIN_CERT, 'utf-8'),
-        decryptionPvk: Fs.readFileSync(DECRYPT_KEY, 'utf-8'),
+        logoutUrl: 'https://adfs.taskworld.com/adfs/ls/?wa=wsignout1.0',
+        // logoutCallbackUrl: 'https://adfs-test.taskworld.com:8000/adfs/ls/?wa=wsignout1.0',
+        issuer: 'taskworld_saml',
+        // cert: Fs.readFileSync(ONELOGIN_CERT, 'utf-8'),
+        // decryptionPvk: Fs.readFileSync(DECRYPT_KEY, 'utf-8'),
+        // logoutCallbackUrl: 'https://adfs-test.taskworld.com:8000/adfs/ls/?wa=wsignout1.0',
         signatureAlgorithm: 'sha256',
         acceptedClockSkewMs: 60000,
       }
@@ -29,6 +33,10 @@ function getServer () {
 
   server.connection({
     port: PORT,
+    tls: {
+      key: Fs.readFileSync('/Users/chanon/tw-secrets/taskworld-godaddy.key'),
+      cert: Fs.readFileSync('/Users/chanon/tw-secrets/taskworld-godaddy.crt')
+    }
   })
 
   server.register(plugins, (err) => {
